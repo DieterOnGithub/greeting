c68a5886a5486b01053652a57c63478b9ec8dfc8
DieterOnGithub <dieter@kribbel.at>
2024-10-25 11:29:53 +0200

    refactoring, adding comments to pithy.h

--- pithy.h
+++ pithy.h
@@ -2,50 +2,52 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
 #include "env.h"
  
 #define BSIZE 256
 
 int pithy(void)  {
+    // set up variables to read in pithy text file
     FILE *fp;
     char buffer[BSIZE];
     char *line;
 
     fp = fopen(filename, "r");
 
+    // generate error message if file reading not possible and exit program
     if ( !fp ) {
         fprintf(stderr, "Unable to open the file %s!\n", filename);
         return 1;
     }
 
+    // initialize array of 50 strings of size BSIZE to store the lines of the file in
     char lines[50][BSIZE];
     int counter = 0;
 
     while ( !feof(fp) ) {
         line = fgets(buffer, BSIZE, fp );
         
+        // stop reading when no more file lines left
         if ( line==NULL) {
             break;
         }
 
+        // add current file line as new element to array
         strcpy(lines[counter], line);
         counter++;
     }
 
     fclose(fp);
 
-    counter = 0;
-    char temp[BSIZE];
+    // seed rand function with current time
+    srand(time(NULL));
 
-    while (true) {
-        printf("Counter is %i\n", counter);
-        strcpy(temp, lines[counter]);
-        if (temp[0] == '\0') {
-            break;
-        }
-        printf("%s", temp);
-        counter++;
-    } 
+    // generate random number to pick line/string from array
+    int index = rand() % (counter + 1);
+
+    // print out line from array
+    printf("Wise line for today: %s", lines[index]);
 
     return 0;
 }
